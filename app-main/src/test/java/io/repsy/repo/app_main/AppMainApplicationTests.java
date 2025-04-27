6ba45e33025dde4c87bc6baecd13e04da7a2a90a
Esma Nur AKSOY <esmaksyy@gmail.com>
2025-04-27 03:17:42 +0300

    A basic test class included

---
+++ app-main/src/test/java/io/repsy/repo/app_main/AppMainApplicationTests.java
@@ -0,0 +1,13 @@
+package io.repsy.repo.app_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
